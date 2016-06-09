ccbfcaa90da06a2b2d0aaae1cd6dd4f97a868c22
Sam Brennan <sam.brennan@talktopebble.co.uk>
2016-06-09 17:30:05 +0100

    Added more tests for monitor manager

--- monitor/test_monitor_manage.py
+++ monitor/test_monitor_manage.py
@@ -16,11 +16,37 @@ class TestParseConfig(unittest.TestCase):
                   "domains:\n"\
                   "- url: 8.8.8.8\n"
 
+    yaml_config2 = "---\n" \
+                   "domains:\n" \
+                   "- url: 8.8.8.8\n" \
+                   "sites:\n" \
+                   "- url: example.uk\n" \
+                   "  status_code: 200\n" \
+                   "- url: example.co.uk\n" \
+                   "  status_code: 302\n" \
+
+
     @patch('monitor_manager.MonitorManager.get_yaml_config')
     def test_read_sites_from_config(self, mock_get_yaml_config):
         """Checks that a config file is correctly read in.
         """
+        # Using yaml_config
         mock_get_yaml_config.return_value = yaml.load(self.yaml_config)
+        monitor_manager = MonitorManager()
+
+        sites = monitor_manager.sites
+
+        for site in sites:
+            self.assertTrue(isinstance(site, Site))
+
+        self.assertEqual(sites[0].url, 'example.uk')
+        self.assertEqual(sites[0].expected_status_code, 200)
+
+        self.assertEqual(sites[1].url, 'example.co.uk')
+        self.assertEqual(sites[1].expected_status_code, 302)
+
+        # Using yaml_config2
+        mock_get_yaml_config.return_value = yaml.load(self.yaml_config2)
         monitor_manager = MonitorManager()
 
         sites = monitor_manager.sites
@@ -44,6 +70,43 @@ class TestParseConfig(unittest.TestCase):
         sites = monitor_manager.sites
 
         self.assertEqual(sites, [])
+
+    @patch('monitor_manager.MonitorManager.get_yaml_config')
+    def test_read_domains_from_config(self, mock_get_yaml_config):
+        """Checks that a config file is correctly read in.
+        """
+        # Using yaml_config
+        mock_get_yaml_config.return_value = yaml.load(self.yaml_config)
+        monitor_manager = MonitorManager()
+
+        domains = monitor_manager.domains
+
+        for domain in domains:
+            self.assertTrue(isinstance(domain, str))
+
+        self.assertEqual(domains[0], '8.8.8.8')
+
+        # Using yaml_config2
+        mock_get_yaml_config.return_value = yaml.load(self.yaml_config)
+        monitor_manager = MonitorManager()
+
+        domains = monitor_manager.domains
+
+        for domain in domains:
+            self.assertTrue(isinstance(domain, str))
+
+        self.assertEqual(domains[0], '8.8.8.8')
+
+    @patch('monitor_manager.MonitorManager.get_yaml_config')
+    def test_read_domains_from_blank_config(self, mock_get_yaml_config):
+        """Attempts to read from an empty config file
+        """
+        mock_get_yaml_config.return_value = yaml.load("")
+        monitor_manager = MonitorManager()
+
+        domains = monitor_manager.domains
+
+        self.assertEqual(domains, [])
 
 if __name__ == '__main__':
     unittest.main()
