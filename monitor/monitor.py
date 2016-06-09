25299e3a353442b2d85924aa1094080803a2e476
Sam Brennan <sam.brennan@talktopebble.co.uk>
2016-06-09 11:44:02 +0100

    Added the monitor file with the Monitor class

---
+++ monitor/monitor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Used to monitor the status of a website.
+"""
+
+import requests
+
+
+class Monitor:
+
+    def __init__(self, url="example.com", expected_status_code=200):
+        self.url = url
+        self.expected_status_code = expected_status_code
+        self.status_code = self.get_status_code()
+
+
+    def check_status_code(self):
+        if self.expected_status_code == self.status_code:
+            return True
+        else:
+            return False
+
+    def get_status_code(self):
+        return requests.get(url=self.url).status_code
