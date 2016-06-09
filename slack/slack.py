3ffd12d6a8af9d1cea39bd0c8e1fca70744e0a8b
Sam Brennan <sam.brennan@talktopebble.co.uk>
2016-06-09 11:08:39 +0100

    Moved slack into a class

--- slack/slack.py
+++ slack/slack.py
@@ -7,26 +7,26 @@ Sends a message to Slack using the chat.postMessage API call.
 from slacker import Slacker
 
 
-slack_api_token = 'xoxb-49168260642-Ex0cis6udHxPAHjWvB2HoQy2'  # The bots token
-slack_username = "@systems.monitor"
-slack_channel = '@sam.brennan'  # This would probably be the status channel
-slack_emote = ':scream:'
+class Slack:
+    slack_api_token = 'xoxb-49168260642-Ex0cis6udHxPAHjWvB2HoQy2'  # The bots token
+    slack_username = "@systems.monitor"
+    slack_channel = '@sam.brennan'  # This would probably be the status channel
+    slack_emote = ':scream:'
 
-slack_text = 'This is a test system state message, please ignore me.'
+    slack_text_prefix = 'System Error @sam.brennan: '
 
+    slack = Slacker(slack_api_token)
 
-def post_message(slack, text):
-    if slack:
-        slack.chat.post_message(channel=slack_channel,
-                                text=text,
-                                username=slack_username,
-                                icon_emoji=slack_emote)
+    def post_message(self, text):
+        self.slack.chat.post_message(channel=self.slack_channel,
+                                     text=''.join((self.slack_text_prefix, text)),
+                                     username=self.slack_username,
+                                     icon_emoji=self.slack_emote)
 
 
 def main():
-    slack = Slacker(slack_api_token)
-
-    post_message(slack, slack_text)
+    slack = Slack()
+    slack.post_message('This is a test system state message, please ignore me.')
 
 
 if __name__ == '__main__':
