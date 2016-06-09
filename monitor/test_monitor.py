46e7f5e5e6b641750460590ac5a2875215079fc4
Sam Brennan <sam.brennan@talktopebble.co.uk>
2016-06-09 11:43:16 +0100

    Updated monitor test

--- monitor/test_monitor.py
+++ monitor/test_monitor.py
@@ -1,15 +1,23 @@
 import unittest
 
-from monitor import get_status_code
+from mock import patch
+
+from monitor import Monitor
 
 
 class TestMonitor(unittest.TestCase):
 
-    def test_get_200_ok_status_code(self):
+    @patch('monitor.requests')
+    def test_get_200_ok_status_code(self, mock_requests):
         """This checks that the function returns the 200 status code
          when a GET request to a valid url is sent
         """
-        url = "200ok.com"
-        status_code = get_status_code(url)
+        mock_requests.get.return_value.status_code = 200
 
-        self.assertEqual(status_code, 200)
+        url = "http://example.com"
+        expected_status_code = 200
+        monitor = Monitor(url=url, expected_status_code=expected_status_code)
+
+        status_code = monitor.get_status_code()
+
+        self.assertEqual(status_code, expected_status_code)
