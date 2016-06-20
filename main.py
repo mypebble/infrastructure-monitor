1761075403767a7c6d0a65f0be70106c958f3c15
Sam Brennan <sam.brennan@talktopebble.co.uk>
2016-06-20 10:23:13 +0100

    Added Sentry support for exceptions in main.py

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import logging
 
 from monitor.monitor_manager import MonitorManager
 
+from raven import Client
+
+
+client = Client('https://7b44b294b11c4b5a88a69c22df64b480:6934c880b80f4e6aa14e'
+                'b9513a3e65b6@app.getsentry.com/83436')
 
 logging.basicConfig(filename='infrastructure-monitor.log', level=logging.INFO)
 
@@ -16,23 +21,27 @@ logging.basicConfig(filename='infrastructure-monitor.log', level=logging.INFO)
 def main(argv=None):
     try:
         opts, args = getopt.getopt(argv, "hc:s:", ["config=", "slackconfig="])
-    except getopt.GetoptError:
+    except getopt.GetoptError as e:
         print('main.py -c <configfile> -s <slack-configfile>')
+
+        logging.error(u"Invalid parameters passed in: ".format(
+            error=unicode(e.message)))
+        client.captureException()
         sys.exit(2)
 
-    config = None
-    slack_config = None
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print('main.py -c <configfile> -s <slack-configfile>')
-            sys.exit()
-        elif opt in ("-c", "--config"):
-            config = arg
-        elif opt in ("-s", "--slack-configfile"):
-            slack_config = arg
-
     try:
+        config = None
+        slack_config = None
+
+        for opt, arg in opts:
+            if opt == '-h':
+                print('main.py -c <configfile> -s <slack-configfile>')
+                sys.exit()
+            elif opt in ("-c", "--config"):
+                config = arg
+            elif opt in ("-s", "--slack-configfile"):
+                slack_config = arg
+
         # There must be a nicer way of doing this...
         if config and slack_config:
             manager = MonitorManager(config_file=config,
@@ -49,9 +58,9 @@ def main(argv=None):
     except KeyboardInterrupt as e:
         sys.exit(e.message)
     except Exception as e:
-        # TODO - Raise something to Sentry
         logging.error(u"Exiting from an error:\n{error}".format(
             error=unicode(e.message)))
+        client.captureException()
         sys.exit(e.message)
 
 if __name__ == '__main__':
